0e604eb3c38d86c7dd59f5db9e7d77ee56cd062d
olyvka <oliamagir06@gmail.com>
2024-08-18 12:06:46 +0300

    not found fix

--- src/server.js
+++ src/server.js
@@ -30,6 +30,13 @@ export const setupServer = () => {
   app.get('/contacts/:contactId', async (req, res) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
+    
+    if (!contact) {
+      return res.status(404).json({
+        message: 'Contact not found',
+      });
+    }
+
     res.status(200).json({
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
